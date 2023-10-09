b35fe63b86682b8977c9856f9ac5d711e797d6f9
Jagan26042000 <jagan26042000@gmail.com>
2023-10-09 13:51:17 +0530

    saving app js file

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navigationbar from "./components/Navigatoionbar";
+import Gallery from "./components/Gallery";
+import Features from "./components/Features";
+import StudentFeedback from "./components/StudentsFeedback";
+import Footer from "./components/Footer";
+import FrequentlyAskedQuestions from "./components/FrequentlyAskedQuestions";
+import ContactUs from "./components/ContactUs";
+function App() {
+  return (
+    <div>
+      <Navigationbar></Navigationbar>
+      
+      <Features /><br /><br /><br /><br /><br />
+      <Gallery /><br /><br /><br /><br /><br />
+      <StudentFeedback /><br /><br /><br /><br /><br />
+      <FrequentlyAskedQuestions /><br /><br /><br /><br /><br />
+      <ContactUs /><br />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
