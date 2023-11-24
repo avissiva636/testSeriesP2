3e93f052cc44ec63ea52b2400e7fe7a08df8bbf6
sivalingam <lingamsiva636@gmail.com>
2023-11-25 04:37:33 +0530

    Db correction

--- Backend/src/database/models/courseModel.js
+++ Backend/src/database/models/courseModel.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
     Title: String,
-    subarr: [Object], // Array of subarr subdocuments
+    Description: [Object], // Array of Description subdocuments
     SubTitle: [
         {
             Title: String,
-            subarr: [Object], // Array of subarr subdocuments
+            Description: [Object], // Array of Description subdocuments
         },
     ],
 });
 
 courseSchema.pre('validate', function () {
-    if ((this.subarr.length > 0 && this.SubTitle.length > 0) ||
-        (this.subarr.length < 0 && this.SubTitle.length < 0)
+    if ((this.Description.length > 0 && this.SubTitle.length > 0) ||
+        (this.Description.length < 0 && this.SubTitle.length < 0)
     ) {
-        this.invalidate('subarr', 'Only one of subarr or SubTitle should exist for a document');
-        this.invalidate('SubTitle', 'Only one of subarr or SubTitle should exist for a document');
+        this.invalidate('Description', 'Only one of Description or SubTitle should exist for a document');
+        this.invalidate('SubTitle', 'Only one of Description or SubTitle should exist for a document');
     }
 
 });
