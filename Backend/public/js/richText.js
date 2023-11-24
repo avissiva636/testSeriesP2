fca271422f6712e5afd0d0da7a252b295702ec6b
sivalingam <lingamsiva636@gmail.com>
2023-11-24 16:45:22 +0530

    update richtext

--- Backend/public/js/richText.js
+++ Backend/public/js/richText.js
@@ -10,13 +10,13 @@ var quillDes;
 document.addEventListener('DOMContentLoaded', function () {
   document.body.addEventListener('click', function (event) {
     // Check if the clicked element has a specific class or ID
-    if (event.target.id === 'noSubtitle') {      
+    if (event.target.id === 'noSubtitle') {
       var quillEditorNormal = document.getElementById('quill-editornormal');
       quillEditorNormal.innerHTML = '';
       const editor = document.createElement("div");
       quillEditorNormal.append(editor);
 
-      if (quillEditorNormal) {        
+      if (quillEditorNormal) {
         quillNormal = new Quill(editor, {
           theme: 'snow',
           modules: {
@@ -38,14 +38,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   document.body.addEventListener('click', function (event) {
-    if (event.target.id === 'yesSubtitle') {    
+    if (event.target.id === 'yesSubtitle') {
       console.log("first")
       var quillEditorSub = document.getElementById('quill-editorSub');
       quillEditorSub.innerHTML = '';
       const editor = document.createElement("div");
       quillEditorSub.append(editor);
 
-      if (quillEditorSub) {        
+      if (quillEditorSub) {
         quillSub = new Quill(editor, {
           theme: 'snow',
           modules: {
@@ -68,13 +68,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.body.addEventListener('click', function (event) {
     // Check if the clicked element has a specific class or ID
-    if (event.target.id === 'course') {   
+    if (event.target.id === 'course') {
       var quillEditorUpAdd = document.getElementById('quill-editorSubupAdd');
       quillEditorUpAdd.innerHTML = '';
       const editor = document.createElement("div");
       quillEditorUpAdd.append(editor);
 
-      if (quillEditorUpAdd) {        
+      if (quillEditorUpAdd) {
         quillUpAdd = new Quill(editor, {
           theme: 'snow',
           modules: {
@@ -97,13 +97,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.body.addEventListener('click', function (event) {
     // Check if the clicked element has a specific class or ID
-    if (event.target.id === 'course') {   
+    if (event.target.id === 'course') {
       var quillEditorUp = document.getElementById('quill-editorSubup');
       quillEditorUp.innerHTML = '';
       const editor = document.createElement("div");
       quillEditorUp.append(editor);
 
-      if (quillEditorUp) {        
+      if (quillEditorUp) {
         quillUp = new Quill(editor, {
           theme: 'snow',
           modules: {
@@ -126,57 +126,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.body.addEventListener('click', function (event) {
     // Check if the clicked element has a specific class or ID
-    if (event.target.id === 'course') {   
-      var quillEditorDes = document.getElementById('quillUpdateDescription');
-      quillEditorDes.innerHTML = '';
-      const editor = document.createElement("div");
-      quillEditorDes.append(editor);
+    if (event.target.id === 'updateCourseQload') {
+      var contentDiv = document.getElementById('content');
+      
+        function loadQuillDes() {
+          try {           
 
-      if (quillEditorDes) {        
-        quillDes = new Quill(editor, {
-          theme: 'snow',
-          modules: {
-            toolbar: [
-              [{ header: [1, 2, 3, 4, 5, 6, false] }],
-              [{ font: [] }],
-              [{ list: "ordered" }, { list: "bullet" }],
-              ["bold", "italic", "underline"],
-              [{ color: [] }, { background: [] }],
-              [{ script: "sub" }, { script: "super" }],
-              [{ align: [] }],
-              ["image", "blockquote", "code-block"],
-              ["clean"],
-            ],
-          },
-        });
-      }
+            var quillEditorDes = document.getElementById('quillUpdateDescription');
+            quillEditorDes.innerHTML = '';
+            const editor = document.createElement("div");
+            quillEditorDes.append(editor);
+    
+            if (quillEditorDes) {
+              quillDes = new Quill(editor, {
+                theme: 'snow',
+                modules: {
+                  toolbar: [
+                    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                    [{ font: [] }],
+                    [{ list: "ordered" }, { list: "bullet" }],
+                    ["bold", "italic", "underline"],
+                    [{ color: [] }, { background: [] }],
+                    [{ script: "sub" }, { script: "super" }],
+                    [{ align: [] }],
+                    ["image", "blockquote", "code-block"],
+                    ["clean"],
+                  ],
+                },
+              });
+            }            
+          } catch (error) {
+            contentDiv.removeEventListener('change', loadQuillDes);
+          }
+        }        
+      
+      contentDiv.addEventListener('change', loadQuillDes);
+
     }
   });
 
 });
-
-
-// var quillEditorSub = document.getElementById('quill-editorSub');
-// document.addEventListener('DOMContentLoaded', function () {
-//   if (quillEditorSub) {
-//     console.log("second")
-//     quillSub = new Quill(quillEditorSub, {
-//       theme: 'snow',
-//       modules: {
-//         toolbar: [
-//           [{ header: [1, 2, 3, 4, 5, 6, false] }],
-//           [{ font: [] }],
-//           [{ list: "ordered" }, { list: "bullet" }],
-//           ["bold", "italic", "underline"],
-//           [{ color: [] }, { background: [] }],
-//           [{ script: "sub" }, { script: "super" }],
-//           [{ align: [] }],
-//           ["image", "blockquote", "code-block"],
-//           ["clean"],
-//         ],
-//       },
-//     });
-//   }
-
-// });
-
