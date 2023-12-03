2592249af2fc730082318b8302e394365c6e5363
Jagan26042000 <jagan26042000@gmail.com>
2023-12-04 00:39:01 +0530

    Minor changes

--- Frontend/src/components/ResultPage.js
+++ Frontend/src/components/ResultPage.js
@@ -1,56 +1,59 @@
 import ContactUsHomePage from "./ContactUsHomePage";
 import Footer from "./Footer";
-import icon from "./css/images/geography.png"
-import medal from "./css/images/medal.png"
-import white from "./css/images/whiteboard.png"
-import mortar from "./css/images/mortarboard.png"
+import icon from "./css/images/geography.png";
+import medal from "./css/images/medal.png";
+import white from "./css/images/whiteboard.png";
+import mortar from "./css/images/mortarboard.png";
 
 const ResultPage = () => {
   return (
     <div>
+      <div style={{marginLeft: "600px", marginTop: "10px"}}>
+        <h1>Our Results</h1>
+      </div>
       <div class="container result__container mt-4">
-  <div class="row">
-    <div class="col-md-3">
-      <div class="card__result">
-        <div class="card-body ">
-          <img className="result__icon" src={medal} alt="icon" />
-<div className="results__marks py-2">1000&#43;</div>
-          <p class="card-text">Some text for Card 1.</p>
-        </div>
-      </div>
-    </div>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card__result">
+              <div class="card-body ">
+                <img className="result__icon" src={medal} alt="icon" />
+                <div className="results__marks py-2">15&#43;</div>
+                <p class="card-text">Year of Experiance</p>
+              </div>
+            </div>
+          </div>
 
-    <div class="col-md-3">
-      <div class="card__result">
-        <div class="card-body">
-          <img className="result__icon" src={icon} alt="icon" />
-          <div className="results__marks py-2">1000&#43;</div>
-          <p class="card-text">Some text for Card 2.</p>
-        </div>
-      </div>
-    </div>
+          <div class="col-md-3">
+            <div class="card__result">
+              <div class="card-body">
+                <img className="result__icon" src={icon} alt="icon" />
+                <div className="results__marks py-2">6000&#43;</div>
+                <p class="card-text">Total Students</p>
+              </div>
+            </div>
+          </div>
 
-    <div class="col-md-3">
-      <div class="card__result">
-        <div class="card-body">
-        <img className="result__icon" src={white} alt="icon" />
-        <div className="results__marks py-2">1000&#43;</div>
-          <p class="card-text">Some text for Card 3.</p>
-        </div>
-      </div>
-    </div>
+          <div class="col-md-3">
+            <div class="card__result">
+              <div class="card-body">
+                <img className="result__icon" src={white} alt="icon" />
+                <div className="results__marks py-2">1000&#43;</div>
+                <p class="card-text">Students Placed</p>
+              </div>
+            </div>
+          </div>
 
-    <div class="col-md-3">
-      <div class="card__result">
-        <div class="card-body">
-        <img className="result__icon" src={mortar} alt="icon" />
-        <div className="results__marks py-2">1000&#43;</div>
-          <p class="card-text">Some text for Card 4.</p>
+          <div class="col-md-3">
+            <div class="card__result">
+              <div class="card-body">
+                <img className="result__icon" src={mortar} alt="icon" />
+                <div className="results__marks py-2">15&#43;</div>
+                <p class="card-text">Total Courses</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
       <ContactUsHomePage />
     </div>
   );
