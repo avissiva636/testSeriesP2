266f6e0b0f82a3056b45edc8cf360dbe9fd57881
sivalingam <lingamsiva636@gmail.com>
2023-12-20 23:21:18 +0530

    Remove Unwanted Code Make async server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,43 +1,43 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const errorHandler = require("./util/middleware/errorHandler");
-const {connectDb} = require("./database");
+const { connectDb } = require("./database");
 const path = require("path");
 const ejs = require('ejs');
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
 
-const app = express();
+const initializeServer = async () => {
+    const app = express();
 
-port = process.env.PORT || 5000;
+    port = process.env.PORT || 5000;
 
-connectDb();
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../public/views"));
+    await connectDb();
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "../public/views"));
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 
-const publicPath = path.join(__dirname, '..', 'public/js');
-console.log(publicPath)
-// app.use("public",express.static( publicPath));
-// app.use(express.static(publicPath));
-app.use(express.static(path.join(__dirname, '../public')));
+    app.use(express.static(path.join(__dirname, '../public')));
 
 
-app.use("/",require("./routes/adminRoutes"));
-app.use("/course",require("./routes/courseRoutes"));
-app.use("/product",require("./routes/productRoutes"));
-app.use("/gallary",require("./routes/gallaryRoutes"));
-app.use("/testimonial",require("./routes/testimonialRoutes"));
-app.use("/notification",require("./routes/notificationRoutes"));
+    app.use("/", require("./routes/adminRoutes"));
+    app.use("/course", require("./routes/courseRoutes"));
+    app.use("/product", require("./routes/productRoutes"));
+    app.use("/gallary", require("./routes/gallaryRoutes"));
+    app.use("/testimonial", require("./routes/testimonialRoutes"));
+    app.use("/notification", require("./routes/notificationRoutes"));
 
-app.use("/api/home",require("./routes/contactRoutes"));
-app.use("/api/users",require("./routes/userRoutes"));
-app.use(errorHandler);
+    app.use("/api/home", require("./routes/contactRoutes"));
+    app.use("/api/users", require("./routes/userRoutes"));
+    app.use(errorHandler);
 
-app.listen(port,()=>{
-    console.log(`Server runnig on port ${port}`)
-});
+    app.listen(port, () => {
+        console.log(`Server runnig on port ${port}`)
+    });
+};
+
+initializeServer();
