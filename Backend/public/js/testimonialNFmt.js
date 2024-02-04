9fe3a4859a6f3863d5a000c763f3a1553e77312a
sivalingam <lingamsiva636@gmail.com>
2024-02-04 15:59:59 +0530

    New Format Testimonial in UI

---
+++ Backend/public/js/testimonialNFmt.js
@@ -0,0 +1,59 @@
+function uploadTestimonialFile() { //uploadFile
+    const form = document.getElementById('uploadTestimonialForm');
+    const formData = new FormData(form);
+    // Check if a file is selected
+    const fileInput = document.getElementById('fileTestimonialInput');
+    if (fileInput.files.length < 1) {
+        return
+    }
+
+    fetch(`${testimonialPath}/uploadAddTestimonial`, {
+        method: 'POST',
+        body: formData,
+        withCredentials: true,
+    })
+        .then(response => response.json())
+        .then(data => {
+            // You can update the UI or show a success message here
+            // form.reset();
+            loadSection('addTestimonial')
+            // updateImageGallery(data.files);
+
+        })
+        .catch(error => {
+            // Handle errors or show an error message            
+            switch (error.message) {
+                case '401':
+                    location.reload();
+                    console.log("error");
+                    break;
+                default:
+                    console.log(error.message);
+                    break;
+            }
+        });
+}
+
+
+function deleteTestimonial(deleteFile) {  //deleteImage
+    const formData = new FormData();
+    formData.append("imagenameToDelete", deleteFile);
+    fetch(`${testimonialPath}/deleteTestimonial`, {
+        method: 'DELETE',
+        body: formData,
+        withCredentials: true,
+    })
+        .then(() => loadSection('addTestimonial'))
+        .catch(error => {
+            switch (error.message) {
+                case '401':
+                    location.reload();
+                    console.log("error");
+                    break;
+                default:
+                    console.log(error.message);
+                    break;
+            }
+        });
+
+}
